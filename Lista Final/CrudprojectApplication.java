6c2415b218c7555e23adb86829b149c3dbb11397
juliana <91280784+julianacristina09@users.noreply.github.com>
2026-07-01 18:51:05 -0300

    Add files via upload

---
+++ Lista Final/CrudprojectApplication.java
@@ -0,0 +1,12 @@
+package com.example.crudproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudprojectApplication.class, args);
+    }
+}
---
+++ Lista Final/CrudprojectApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.crudproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
