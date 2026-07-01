d6371014bc4026517adbc59773e65836856f69a1
juliana <91280784+julianacristina09@users.noreply.github.com>
2026-07-01 18:57:36 -0300

    Add files via upload

---
+++ Lista Final/repository/OrcamentoRepository.java
@@ -0,0 +1,12 @@
+package com.example.crudproject.repository;
+
+import com.example.crudproject.model.Orcamento;
+import com.example.crudproject.model.StatusOrcamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrcamentoRepository extends JpaRepository<Orcamento, Long> {
+
+    List<Orcamento> findByStatus(StatusOrcamento status);
+}
