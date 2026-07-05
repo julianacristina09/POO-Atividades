639662e8f2d8e46a3afbbd6aae1d17011be5f64c
juliana <91280784+julianacristina09@users.noreply.github.com>
2026-07-05 15:44:33 -0300

    Add files via upload

---
+++ Jogo da Velha/JogoVelhaApplication.java
@@ -0,0 +1,12 @@
+package com.exemplo.jogovelha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogoVelhaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JogoVelhaApplication.class, args);
+    }
+}
